e3ec91d9ebd70aba645bae47f515ed30bdd242db
Prancingkiller <dimste18@hotmail.com>
2023-09-21 02:43:44 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
-import bootstrap from 'bootstrap'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faNoteSticky,faCalendar,faCirclePlus,faPenToSquare,faCaretLeft } from '@fortawesome/free-solid-svg-icons'
 library.add(faNoteSticky,faCalendar,faCirclePlus,faPenToSquare,faCaretLeft)
 
-createApp(App).use(router).use(bootstrap).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
